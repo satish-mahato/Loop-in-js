43614c89eac9b51510d8ede1e21244dc9f5140b5
satish7654 <satishsingh202dd+@gmail.com>
2024-10-19 09:32:52 +0545

    switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+function switchCase(name) {
+  let = name;
+  switch (name) {
+    case "satish":
+      console.log("this is satish switch");
+      break;
+
+    case "ram":
+      console.log("this is ram switch");
+      break;
+    case "hari":
+      console.log("hari case");
+      break;
+    default:
+      console.log("Invalid case");
+  }
+}
+switchCase("ram");
